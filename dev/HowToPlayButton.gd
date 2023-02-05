1c72e360b28b65a4635b6ea8b18cc72807efebb4
Justin Kuhn <justinkuhn@t-online.de>
2023-02-05 17:37:14 +0100

    Add how to play menu

---
+++ dev/HowToPlayButton.gd
@@ -0,0 +1,9 @@
+extends Button
+
+func _ready():
+	self.connect("pressed", self, "on_pressed")
+
+func on_pressed():
+	get_node("../../..").visible = false
+	var test = get_node("../../../../HowToPlay")
+	test.visible = true
